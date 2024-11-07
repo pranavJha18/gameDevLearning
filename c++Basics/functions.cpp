da99ab00c31070d54b5fe249c9c4865e90f5730a
pranavJha18 <pranavjha5000@gmail.com>
2024-11-07 20:32:26 +0530

    added functions

---
+++ c++Basics/functions.cpp
@@ -0,0 +1,44 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// this creates a function called printHelloWorld
+void printHelloWorld() { // it is called function signature
+  // body of the function
+  std::cout << "Hello World \n";
+}
+// the order of the functions do matter so if we want to call any function A
+// inside any other function B then A shoud be defined before B
+void printMsg(
+    std::string msg,
+    bool newLine) { // this function takes one argument as a string type
+  std::cout << msg;
+  if (newLine) {
+    std::cout << "\n";
+  }
+  printHelloWorld();
+  std::cout << "\n";
+}
+
+std::vector<int> generateRandomNumbers(int range) {
+  std::vector<int> randomNumbers;
+  std::srand(std::time(nullptr));
+
+  for (int i = 0; i < range; i++) {
+    int number = std::rand() % 100 - 50;
+    randomNumbers.push_back(number);
+    std::cout << number << " ";
+  }
+
+  std::cout << "\n";
+  return randomNumbers;
+}
+
+int main(int argc, char *argv[]) { // this is the entry point of the program
+  printHelloWorld();
+  printMsg("Golu is my name", true);
+  std::vector<int> randNum = generateRandomNumbers(20);
+  std::vector<int> randNum2 = generateRandomNumbers(10);
+  return 0;
+}
