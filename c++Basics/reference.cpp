d0c3e644766deb6c611004c3c2cc4f746a1f0651
pranavJha18 <pranavjha5000@gmail.com>
2024-11-08 13:06:17 +0530

    added reference

---
+++ c++Basics/reference.cpp
@@ -0,0 +1,50 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+std::vector<int> generateRandomNumbers(int range) {
+  std::vector<int> randomNumbers;
+  std::srand(std::time(nullptr));
+
+  for (int i = 0; i < range; i++) {
+    int number = std::rand() % 100 - 50;
+    randomNumbers.push_back(number);
+    std::cout << number << " ";
+  }
+
+  std::cout << "\n";
+  return randomNumbers;
+}
+
+// & means taking arguments as a reference
+// not having & means pass by value/copy, with & means pass by reference
+// missing a const means that the variable can be altered
+// modifier
+void sortVector(std::vector<int> &vectorToSort) { // does alter the value
+  for (int i = 0; i < vectorToSort.size(); i++) {
+    for (int j = i + 1; j < vectorToSort.size(); j++) {
+      if (vectorToSort[i] > vectorToSort[j]) {
+        std::swap(vectorToSort[i], vectorToSort[j]);
+      }
+    }
+  }
+}
+
+// accessor only
+void printVector(
+    const std::vector<int> &printvect) { // only reads the arguments
+  for (int num : printvect) {
+    std::cout << num << " ";
+  }
+}
+
+int main(int argc, char *argv[]) { // this is the entry point of the program
+  std::vector<int> randNum = generateRandomNumbers(20);
+  //  std::vector<int> sorted = sortVector(randNum);
+
+  sortVector(randNum);
+  printVector(randNum);
+  std::cout << "\n";
+  return 0;
+}
